761761c105d6218721fae5665c330a8ae0a41b5b
3dd13 <tatonlto@gmail.com>
2011-05-01 19:39:01 +0800

    points to cert directory

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, FACEBOOK_KEY, FACEBOOK_SECRET, {:client_options => {:ssl => {:ca_path => "/usr/lib/ssl"}}}
+    provider :facebook, FACEBOOK_KEY, FACEBOOK_SECRET, {:client_options => {:ssl => {:ca_path => "/usr/lib/ssl/certs"}}}
 end
