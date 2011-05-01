a5a846d275be39cc7df05c635a638114200e0e1f
3dd13 <tatonlto@gmail.com>
2011-05-01 18:39:47 +0800

    try initializer to configure ssl ca path

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+FACEBOOK_KEY = "198021086905866"
+FACEBOOK_SECRET = "75b9e172e31ac137449dc37f0914d457"
+
 # Initialize the rails application
 BestComics::Application.initialize!
